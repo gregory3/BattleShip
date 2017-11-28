84f1c15ed6084c4145ad72f58e7e2b5e1ea19a02
harryg30 <gordenstein30@gmail.com>
2017-11-27 20:29:43 -0500

    user stubs

---
+++ src/User.java
@@ -0,0 +1,33 @@
+
+public class User {
+	private Board board;
+	
+	public User(Board board){
+		this.board = board;
+	}
+	
+	/**
+	 * connects to server
+	 */
+	private void connectToServer(){
+		//STUB
+	}
+	
+	/**
+	 * send a string to the server of the user inputed location
+	 * checks valid move
+	 */
+	public void makeMove(){
+		//STUB
+	}
+	/**
+	 * Set board
+	 * while loop
+	 * turn and wait for other player 
+	 */
+	public void playGame(){
+		//STUB
+	}
+	
+	
+}
